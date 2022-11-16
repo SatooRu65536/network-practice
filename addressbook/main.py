93750660ffe1d8bab3bc4e07da3c0d7f9543c180
SatooRu65536 <ryuto4619@gmail.com>
2022-11-17 00:40:37 +0900

    11-16

--- addressbook/main.py
+++ addressbook/main.py
@@ -1,34 +1,98 @@
 from flask import Flask, request, render_template, jsonify
 import json
+import re
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
 
 
 @app.route('/address', methods=["GET"])
-def address_get():
-    print()
-    print()
-    print('adsf', request.args)
-
+def address_get():  
     p_first_name = request.args.get('fn', None)
     p_last_name = request.args.get('ln', None)
     p_email = request.args.get('em', None)
+    mode = request.args.get('mode', None)
 
     with open('address.json') as f:
         json_data = json.load(f)
 
-    if p_first_name:
-        json_data = list(filter(lambda item: p_first_name.lower()
-                                in item["first_name"].lower(), json_data))
-    if p_last_name:
-        json_data = list(filter(lambda item: p_last_name.lower()
-                                in item["last_name"].lower(), json_data))
-    if p_email:
-        json_data = list(filter(lambda item: p_email.lower()
-                                in item["email"].lower(), json_data))
+    status = {
+        'isError': False,
+        'errorCode': 0,
+        'message': ''
+    }
 
-    return jsonify(json_data)
+    if mode == 'add':
+        email_list = [d['email'] for d in json_data]
+
+        # 入力データが不足している
+        if not (p_first_name and p_last_name and p_email):
+            status = {
+                'isError': True,
+                'errorCode': 400,
+                'message': '全て入力してください'
+            }
+
+        elif not re.match('[\s\S]@[\s\S]', p_email):
+            status = {
+                'isError': True,
+                'errorCode': 401,
+                'message': 'メールアドレスの形式が正しくありません'
+            }
+
+        # email が未登録
+        elif p_email not in email_list:
+            status = {
+                'isError': False,
+                'errorCode': 200,
+                'message': 'アドレスを追加しました'
+            }
+            add_data = {
+                "email": p_email,
+                "first_name": p_first_name,
+                "last_name": p_last_name
+            }
+            json_data.append(add_data)
+            with open('address.json', 'w') as f:
+                f.write(json.dumps(json_data))
+
+        # email が登録済み
+        else:
+            status = {
+                'isError': True,
+                'errorCode': 450,
+                'message': 'アドレスは登録済みです'
+            }
+
+    elif mode == 'search':
+        if p_first_name:
+            json_data = list(filter(lambda item: p_first_name.lower()
+                                    in item["first_name"].lower(), json_data))
+        if p_last_name:
+            json_data = list(filter(lambda item: p_last_name.lower()
+                                    in item["last_name"].lower(), json_data))
+        if p_email:
+            json_data = list(filter(lambda item: p_email.lower()
+                                    in item["email"].lower(), json_data))
+
+        if len(json_data) > 0:
+            status = {
+                'isError': False,
+                'errorCode': 200,
+                'message': f'{len(json_data)}件みつかりました'
+            }
+        else:
+            status = {
+                'isError': True,
+                'errorCode': 451,
+                'message': '見つかりませんでした'
+            }
+
+    recv_data = {
+        'status': status,
+        'body': json_data
+    }
+    return jsonify(recv_data)
 
 
 @app.route('/')
--- addressbook/static/addressbook.js
+++ addressbook/static/addressbook.js
@@ -1,7 +1,7 @@
 fetch("/address").then(response => {
     response.json().then((data) => {
         const tableBody = document.querySelector("#address-list > tbody");
-        data.forEach(elm => {
+        data.body.forEach(elm => {
             let tr = document.createElement('tr');
             let td = document.createElement('td');
             td.innerText = elm.first_name;
@@ -19,6 +19,72 @@ fetch("/address").then(response => {
 });
 
 
+const textBoxes = [...document.getElementsByTagName('input')];
+const message = document.getElementById('message');
+textBoxes.forEach((textBox) => {
+    textBox.addEventListener('input', () => {
+        if (!message.innerText) return;
+        textBoxes.forEach(tb => tb.style.backgroundColor = null);
+        document.getElementById('message').innerText = '';
+    });
+});
+
+
+const errorIndicator = (errorCode) => {
+    const fn = document.getElementById('add-firstname');
+    const ln = document.getElementById('add-lastname');
+    const em = document.getElementById('add-email');
+
+    switch (errorCode) {
+        case 400:
+            if (!fn.value) fn.style.backgroundColor = '#FF9F9F';
+            if (!ln.value) ln.style.backgroundColor = '#FF9F9F';
+            if (!em.value) em.style.backgroundColor = '#FF9F9F';
+            break;
+        case 401:
+        case 450:
+            em.style.backgroundColor = '#FF9F9F';
+            break;
+        case 451:
+            break;
+    }
+}
+
+
+const fetchData = (params) => {
+    fetch("/address?" + params).then(response => {
+        response.json().then((data) => {
+            const tableBody = document.querySelector("#address-list > tbody");
+            const clone = tableBody.cloneNode(false);
+            tableBody.remove();
+            data.body.forEach(elm => {
+                let tr = document.createElement('tr');
+                let td = document.createElement('td');
+                td.innerText = elm.first_name;
+                tr.appendChild(td);
+                td = document.createElement('td');
+                td.innerText = elm.last_name;
+                tr.appendChild(td);
+                td = document.createElement('td');
+                td.innerText = elm.email;
+                tr.appendChild(td);
+
+                clone.appendChild(tr);
+            });
+            document.getElementById('address-list').appendChild(clone);
+
+            message.innerText = data.status.message;
+            if (data.status.isError) {
+                message.style.color = 'red';
+                errorIndicator(data.status.errorCode);
+            } else {
+                message.style.color = 'blue';
+            }
+        });
+    });
+}
+
+
 document.getElementById('search-submit').addEventListener('click', (e) => {
     e.preventDefault();
 
@@ -26,61 +92,20 @@ document.getElementById('search-submit').addEventListener('click', (e) => {
     params.append("fn", document.getElementById('search-firstname').value);
     params.append("ln", document.getElementById('search-lastname').value);
     params.append("em", document.getElementById('search-email').value);
+    params.append("mode", "search");
 
-    fetch("/address?" + params).then(response => {
-        response.json().then((data) => {
-            const tableBody = document.querySelector("#address-list > tbody");
-            const clone = tableBody.cloneNode(false);
-            tableBody.remove();
-
-            data.forEach(elm => {
-                let tr = document.createElement('tr');
-                let td = document.createElement('td');
-                td.innerText = elm.first_name;
-                tr.appendChild(td);
-                td = document.createElement('td');
-                td.innerText = elm.last_name;
-                tr.appendChild(td);
-                td = document.createElement('td');
-                td.innerText = elm.email;
-                tr.appendChild(td);
-
-                clone.appendChild(tr);
-                document.getElementById('address-list').appendChild(clone);
-            });
-        });
-    });
+    fetchData(params);
 });
 
-document.getElementById('add-submit').addEventListener('click', () => {
+
+document.getElementById('add-submit').addEventListener('click', (e) => {
     e.preventDefault();
 
     const params = new URLSearchParams();
-    params.append("fn", document.getElementById('search-firstname').value);
-    params.append("ln", document.getElementById('search-lastname').value);
-    params.append("em", document.getElementById('search-email').value);
+    params.append("fn", document.getElementById('add-firstname').value);
+    params.append("ln", document.getElementById('add-lastname').value);
+    params.append("em", document.getElementById('add-email').value);
+    params.append("mode", "add");
 
-    fetch("/address?" + params).then(response => {
-        response.json().then((data) => {
-            const tableBody = document.querySelector("#address-list > tbody");
-            const clone = tableBody.cloneNode(false);
-            tableBody.remove();
-
-            data.forEach(elm => {
-                let tr = document.createElement('tr');
-                let td = document.createElement('td');
-                td.innerText = elm.first_name;
-                tr.appendChild(td);
-                td = document.createElement('td');
-                td.innerText = elm.last_name;
-                tr.appendChild(td);
-                td = document.createElement('td');
-                td.innerText = elm.email;
-                tr.appendChild(td);
-
-                clone.appendChild(tr);
-                document.getElementById('address-list').appendChild(clone);
-            });
-        });
-    });
+    fetchData(params);
 });
