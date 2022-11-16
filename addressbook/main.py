4ca04ddb88b07f1c431f2ab78eb21fd8b1878c2f
SatooRu65536 <ryuto4619@gmail.com>
2022-11-16 12:44:33 +0900

    11-16 途中

--- addressbook/main.py
+++ addressbook/main.py
@@ -7,24 +7,24 @@ app.config["JSON_AS_ASCII"] = False
 
 @app.route('/address', methods=["GET"])
 def address_get():
+    print()
+    print()
+    print('adsf', request.args)
 
-    # 検索パラメータの取得
     p_first_name = request.args.get('fn', None)
     p_last_name = request.args.get('ln', None)
     p_email = request.args.get('em', None)
-    print("これ", p_first_name, p_last_name, p_email)
 
     with open('address.json') as f:
         json_data = json.load(f)
 
-    # パラメータにより返すデータをフィルタリングする
-    if p_first_name is not None:
+    if p_first_name:
         json_data = list(filter(lambda item: p_first_name.lower()
                                 in item["first_name"].lower(), json_data))
-    if p_last_name is not None:
+    if p_last_name:
         json_data = list(filter(lambda item: p_last_name.lower()
                                 in item["last_name"].lower(), json_data))
-    if p_email is not None:
+    if p_email:
         json_data = list(filter(lambda item: p_email.lower()
                                 in item["email"].lower(), json_data))
 
--- addressbook/static/addressbook.js
+++ addressbook/static/addressbook.js
@@ -1,7 +1,5 @@
 fetch("/address").then(response => {
-    console.log(response);
     response.json().then((data) => {
-        console.log(data);
         const tableBody = document.querySelector("#address-list > tbody");
         data.forEach(elm => {
             let tr = document.createElement('tr');
@@ -24,18 +22,17 @@ fetch("/address").then(response => {
 document.getElementById('search-submit').addEventListener('click', (e) => {
     e.preventDefault();
 
-    const postData = {
-        method: 'POST',
-        body: {
-        'fn': document.getElementById.apply('search-firstname').innerText,
-        'ln': document.getElementById.apply('search-lastname').innerText,
-        'em': document.getElementById.apply('search-email').innerText,
-    }
-    fetch("/address", ).then(response => {
-        console.log(response);
+    const params = new URLSearchParams();
+    params.append("fn", document.getElementById('search-firstname').value);
+    params.append("ln", document.getElementById('search-lastname').value);
+    params.append("em", document.getElementById('search-email').value);
+
+    fetch("/address?" + params).then(response => {
         response.json().then((data) => {
-            console.log(data);
             const tableBody = document.querySelector("#address-list > tbody");
+            const clone = tableBody.cloneNode(false);
+            tableBody.remove();
+
             data.forEach(elm => {
                 let tr = document.createElement('tr');
                 let td = document.createElement('td');
@@ -48,7 +45,41 @@ document.getElementById('search-submit').addEventListener('click', (e) => {
                 td.innerText = elm.email;
                 tr.appendChild(td);
 
-                tableBody.appendChild(tr);
+                clone.appendChild(tr);
+                document.getElementById('address-list').appendChild(clone);
+            });
+        });
+    });
+});
+
+document.getElementById('add-submit').addEventListener('click', () => {
+    e.preventDefault();
+
+    const params = new URLSearchParams();
+    params.append("fn", document.getElementById('search-firstname').value);
+    params.append("ln", document.getElementById('search-lastname').value);
+    params.append("em", document.getElementById('search-email').value);
+
+    fetch("/address?" + params).then(response => {
+        response.json().then((data) => {
+            const tableBody = document.querySelector("#address-list > tbody");
+            const clone = tableBody.cloneNode(false);
+            tableBody.remove();
+
+            data.forEach(elm => {
+                let tr = document.createElement('tr');
+                let td = document.createElement('td');
+                td.innerText = elm.first_name;
+                tr.appendChild(td);
+                td = document.createElement('td');
+                td.innerText = elm.last_name;
+                tr.appendChild(td);
+                td = document.createElement('td');
+                td.innerText = elm.email;
+                tr.appendChild(td);
+
+                clone.appendChild(tr);
+                document.getElementById('address-list').appendChild(clone);
             });
         });
     });
